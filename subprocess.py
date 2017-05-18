bdcb0047e82c56ae40f9a633f0143a2d05e420d2
Manav <manavgarekar@gmail.com>
2017-05-18 10:43:49 -0600

    Create subprocess.py

---
+++ subprocess.py
@@ -0,0 +1,17 @@
+# Basic Subprocess module
+
+import subprocess # TO RUN COMMANDS ON lINUX
+import sys # TO GIVE INPUT TO PYTHON PROGRAMS
+
+if len(sys.argv) !=2:
+    print "Error, Enter the domain name you want to ping"
+    sys.exit()
+
+DOMAIN_TO_PING=sys.argv[1]
+output_code=subprocess.call(["ping",str(DOMAIN_TO_PING),"-c","1","-q","-w","1"])
+
+if output_code==0:
+    print "successful"
+else:
+    print "error"
+    sys.exit()
